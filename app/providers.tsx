f93b17ad6797bf639f3e8a7ff9df57f58035a422
R3Kr <thefungigg@gmail.com>
2023-10-01 11:32:37 +0200

    pusher

--- app/providers.tsx
+++ app/providers.tsx
@@ -3,9 +3,11 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import { CacheProvider } from "@chakra-ui/next-js";
 import { SessionProvider } from "next-auth/react";
+import Pusher from "pusher-js";
 
-import React from "react";
+import React, { useEffect } from "react";
 import { Session } from "next-auth";
+import { useRouter } from "next/navigation";
 
 export default function Providers({
   children,
@@ -14,6 +16,29 @@ export default function Providers({
   children: React.ReactNode;
   session: Session | null;
 }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!process.env.NEXT_PUBLIC_PUSHER_KEY) {
+      throw Error("no pusher key");
+    }
+
+    const pusherInstance = new Pusher(process.env.NEXT_PUBLIC_PUSHER_KEY, {
+      cluster: "eu",
+    });
+    const channel = pusherInstance.subscribe("rickroll");
+
+    channel.bind("rickroll-event", (data: string) => {
+      console.log(data);
+      router.push(data);
+      //redirect("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+    });
+
+    return () => {
+      pusherInstance.unsubscribe("rickroll");
+    };
+  }, []);
+
   return (
     <SessionProvider session={session}>
       <CacheProvider>
