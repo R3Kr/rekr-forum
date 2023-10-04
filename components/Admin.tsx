1de7cca91955a97a08ef3bd7dadb30a3c0abb3a1
R3Kr <thefungigg@gmail.com>
2023-10-04 22:33:53 +0200

    qwe

--- components/Admin.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import { useIsAdmin } from "@/app/providers";
-import React from "react";
-
-export default function Admin() {
-  const admin = useIsAdmin();
-  return <div>{admin ? "Admin" : "User"}</div>;
-}
